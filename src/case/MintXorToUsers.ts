defb427e5789230438934dba057ba9c30b673c7b
Stepan Lavrentev <lawrentievsv@gmail.com>
2021-07-07 10:42:18 +0300

    make mint XOR faster

--- src/case/MintXorToUsers.ts
+++ src/case/MintXorToUsers.ts
@@ -2,18 +2,27 @@ import { KnownAssets, KnownSymbols } from "@sora-substrate/util";
 import { BaseConnection } from "../database/BaseConnection";
 import { PolkaswapUser } from "../database/entity/PolkaswapUser";
 import { LoadUser } from "../userInteraction/LoadUser";
-import { encodeAddress } from '@polkadot/keyring'
-import { mnemonicGenerate, naclKeypairFromSeed, mnemonicToMiniSecret } from '@polkadot/util-crypto'
 import { createArrayOfTransactions, createKeyringFromMnemonic } from '../utils/usefullFunctions'
 import { AssetList, Balances } from "../database";
 
 class MintXorToUsers {
-    public async execute(user: LoadUser): Promise<void> {
-        let i = 1
+    public async execute(loadUser: LoadUser): Promise<void> {
         const database = BaseConnection.getInstance()
         return new Promise(async (finish, _reject) => {
-            while (i > 0) {
-                i-=1
+
+            const asset = KnownAssets.get(KnownSymbols.XOR)
+            const amount = "100000000000000000000"
+
+            console.time('Mint token processing')
+            while (loadUser.iterations > 0) {
+                loadUser.iterations-=1
+                await mintAssetToDBUser()
+                console.timeLog('Mint token processing', `Was completed iteration ${loadUser.iterations}`)
+            }
+            console.log('finish')
+            finish()
+
+            async function mintAssetToDBUser() {
 
                 const userRepo = database.connection.getRepository(PolkaswapUser)
 
@@ -25,19 +34,17 @@ class MintXorToUsers {
                     .limit(1000)
                     .getMany();
 
-                const asset = KnownAssets.get(KnownSymbols.XOR)
-                const amount = "100000000000000000000"
                 const mnemonic = 'era actor pluck voice frost club gallery palm moment empower whale flame'
                 const sudo = createKeyringFromMnemonic('sr25519', mnemonic)
 
                 const faucetTxArray = createArrayOfTransactions(
                     users,
-                    user.api.api.tx.faucet.transfer,
-                    user.api.api.tx.utility.batch,
+                    loadUser.api.api.tx.faucet.transfer,
+                    loadUser.api.api.tx.utility.batch,
                     asset.address, 'address', amount
                 )
                 for (let sendBatch of faucetTxArray) {
-                    await user.api.submitExtrinsic(sendBatch, sudo)
+                    await loadUser.api.submitExtrinsic(sendBatch, sudo)
                 }
                 const manager = database.connection.manager
                 let assetElement = new AssetList()
@@ -45,23 +52,25 @@ class MintXorToUsers {
                 assetElement.name = asset.name
                 assetElement.symbol = asset.symbol
                 assetElement.tokenAddress = asset.address
-                assetElement.tokenOwner = (await user.api.api.query.assets.assetOwners(asset.address)).toString()
+                assetElement.tokenOwner = (await loadUser.api.api.query.assets.assetOwners(asset.address)).toString()
                 await manager.save(assetElement)
                 let balances: any[] = []
                 for (let user of users){
                     const balance = new Balances();
                     balance.balance = amount;
                     balance.assetId = assetElement;
+                    balance.user = user
                     user.hasMoney = true;
-                    user.balances.push(balance);
                     balances.push(balance);
-                    // await manager.save(balance);
-                    await manager.save(user);
                 }
-
+                let promises = users.map(update =>  {
+                    delete update.balances
+                    return manager.update(PolkaswapUser, update.userId, update)
+                })
+                await Promise.all(promises);
+                await manager.insert(Balances, balances);
             }
-            console.log('finish')
-            finish()
+
         })
     }
 }
